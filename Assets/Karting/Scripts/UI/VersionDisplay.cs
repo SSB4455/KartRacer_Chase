62ebcd75c3891088578ce4947c11fffbc2a8f1cd
SSB4455 <741086@gmail.com>
2020-07-03 12:01:32 +0800

    增加简单卡丁车

---
+++ Assets/Karting/Scripts/UI/VersionDisplay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class VersionDisplay : MonoBehaviour
+{
+    public Text m_Text;
+    
+    void Awake()
+    {
+        m_Text.text = $"Version: {Application.version}\n";
+    }
+
+    
+}
