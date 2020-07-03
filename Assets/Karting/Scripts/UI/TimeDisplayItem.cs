c69a67d0e42c929b676c4b0c206313f5ba6b7733
SSB4455 <741086@gmail.com>
2020-07-03 12:01:32 +0800

    增加简单卡丁车

---
+++ Assets/Karting/Scripts/UI/TimeDisplayItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+/// <summary>
+/// A class that handle the title and body of a TimeDisplayItem.
+/// </summary>
+public class TimeDisplayItem : MonoBehaviour
+{
+    [Tooltip("A reference to the Text to display the time.")]
+    [SerializeField]
+    protected Text display;
+
+    [Tooltip("A reference to the Text to display the title for the time.")]
+    [SerializeField]
+    protected Text title;
+    
+    /// <summary>
+    /// Set the text body of the TimeDisplayItem. 
+    /// If the input "text" is null or empty the method will disable the TimeDisplayItem gameobject. Otherwise it enables it.
+    /// </summary>
+    /// <param name="text">string to display in the body</param>
+    public void SetText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        
+        gameObject.SetActive(true);
+        display.text = text;
+    }
+
+    /// <summary>
+    /// Set the text title of the TimeDisplayItem.
+    /// </summary>
+    /// <param name="text">string to display in the title</param>
+    public void SetTitle(string text)
+    {
+        title.text = text;
+    }
+
+}
