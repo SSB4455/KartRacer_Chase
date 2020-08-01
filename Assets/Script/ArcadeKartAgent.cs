d7a9c78abc5d24ebb91639b6dedc5f03d1798b38
SSB4455 <741086@gmail.com>
2020-08-01 22:01:26 +0800

    调整射线检测器的实现及配置

--- Assets/Script/ArcadeKartAgent.cs
+++ Assets/Script/ArcadeKartAgent.cs
@@ -1,11 +1,10 @@
 /*
 SSBB4455 2020-07-03
 */
+using KartGame.KartSystems;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
-using KartGame.KartSystems;
-using System.Collections.Generic;
 
 public class ArcadeKartAgent : Agent, IInput
 {
@@ -18,36 +17,10 @@ public class ArcadeKartAgent : Agent, IInput
 
 	public float hitPenalty = -0.1f;
 
-	#region Senses
+#region Senses
 	[Header("Observation Params")]
-	[Tooltip("How far should the agent shoot raycasts to detect the world?")]
-	public float RaycastDistance;
-	[Tooltip("What objects should the raycasts hit and detect?")]
-	public LayerMask Mask;
-	[Tooltip("Sensors contain ray information to sense out the world, you can have as many sensors as you need.")]
-	public Sensor[] Sensors;
-	/// <summary>
-	/// Sensors hold information such as the position of rotation of the origin of the raycast and its hit threshold
-	/// to consider a "crash".
-	/// </summary>
-	[System.Serializable]
-	public struct Sensor
-	{
-		public Transform Transform;
-		public float HitThreshold;
-	}
-
-	[Header("Checkpoints")]
-	[Tooltip("What are the series of checkpoints for the agent to seek and pass through?")]
-	public Collider[] Colliders;
-	[Tooltip("What layer are the checkpoints on? This should be an exclusive layer for the agent to use.")]
-	public LayerMask CheckpointMask;
-
-	[Space]
-	[Tooltip("Would the agent need a custom transform to be able to raycast and hit the track? " +
-		"If not assigned, then the root transform will be used.")]
-	public Transform AgentSensorTransform;
-	#endregion
+	public ArcadeKartRaycastSensor[] raycastSensors;
+#endregion
 
 	#region Debugging
 	[Header("Debug Option")]
@@ -59,53 +32,26 @@ public class ArcadeKartAgent : Agent, IInput
 
 	public override void OnEpisodeBegin()
 	{
-		//卡丁车位置重置
-		//arcadeKart = GetComponent<ArcadeKart>();
-		if (AgentSensorTransform == null)
-		{
-			AgentSensorTransform = transform;
-		}
+		
 	}
 
 	public override void CollectObservations(VectorSensor sensor)
 	{
 		// Car type
-		sensor.AddObservation(0.2f);
+		sensor.AddObservation(0.1f);
 
 		// Agent velocity
 		sensor.AddObservation(arcadeKart.Rigidbody.velocity);
+
+		AddReward(arcadeKart.LocalSpeed() / 10f);
 
 		// Car forward
 		sensor.AddObservation(arcadeKart.transform.forward);
 
 		// Sensors
-		for (int i = 0; i < Sensors.Length; i++)
+		for (int i = 0; i < raycastSensors?.Length; i++)
 		{
-			var currentSensor = Sensors[i];
-			var xform = currentSensor.Transform;
-			bool hit = Physics.Raycast(AgentSensorTransform.position, xform.forward, out var hitInfo,
-				RaycastDistance, Mask, QueryTriggerInteraction.Ignore);
-
-			if (ShowRaycasts)
-			{
-				Debug.DrawRay(AgentSensorTransform.position, xform.forward * RaycastDistance, Color.green);
-				Debug.DrawRay(AgentSensorTransform.position, xform.forward * RaycastDistance * currentSensor.HitThreshold, Color.red);
-			}
-
-			float hitDistance = (hit ? hitInfo.distance : RaycastDistance) / RaycastDistance;
-
-			List<float> sensorRaycastInfoList = new List<float>();
-			// 检测器指向角度
-			sensorRaycastInfoList.AddRange(new float[3] { xform.eulerAngles.normalized.x, xform.eulerAngles.normalized.y, xform.eulerAngles.normalized.z });
-			sensorRaycastInfoList.Add(hitDistance);     // 碰撞距离
-			sensorRaycastInfoList.Add(currentSensor.HitThreshold);     // 碰撞阈值
-			//sensorRaycastInfoList.Add(hitType);     // 检测到的碰撞类型(墙壁 己方车辆 对方车辆)
-			sensor.AddObservation(sensorRaycastInfoList);
-
-			if (hitDistance < currentSensor.HitThreshold)	//根据减少的速度来设置惩罚
-			{
-				AddReward(hitPenalty);
-			}
+			sensor.AddObservation(raycastSensors[i].GetObservationV1());
 		}
 
 		//比赛完成度 前一辆车比赛完成度 后一辆车比赛完成度
@@ -113,28 +59,6 @@ public class ArcadeKartAgent : Agent, IInput
 		//当前排名 目标排名
 
 		// Road forward (current 10m 20m)
-	}
-
-	public Mesh mesh;
-	public Material material;
-	public void Update()
-	{
-
-		for (int i = 0; i < Sensors.Length; i++)
-		{
-			var current = Sensors[i];
-			var xform = current.Transform;
-			var hit = Physics.Raycast(AgentSensorTransform.position, xform.forward, out var hitInfo,
-				RaycastDistance, Mask, QueryTriggerInteraction.Ignore);
-
-			if (ShowRaycasts)
-			{
-				//Graphics.DrawMesh(mesh, Vector3.zero, Quaternion.identity, material, 0);
-				Debug.DrawRay(AgentSensorTransform.position, xform.forward * RaycastDistance, Color.green);
-				Debug.DrawRay(AgentSensorTransform.position, xform.forward * current.HitThreshold, Color.red);
-			}
-
-		}
 	}
 
 	public override void OnActionReceived(float[] vectorAction)
@@ -167,7 +91,7 @@ public class ArcadeKartAgent : Agent, IInput
 		{
 			lastGroundCollided = other.collider.gameObject;
 			Debug.Log(other.gameObject.name);
-			AddReward(hitPenalty);
+			//AddReward(hitPenalty);
 		}
 
 		//检测碰到终点 计算时间给予奖励
@@ -175,8 +99,8 @@ public class ArcadeKartAgent : Agent, IInput
 
 	void OnTriggerEnter(Collider other)
 	{
-		int maskedValue = 1 << other.gameObject.layer;
-		int triggered = maskedValue & CheckpointMask;
+		//int maskedValue = 1 << other.gameObject.layer;
+		//int triggered = maskedValue & CheckpointMask;
 
 		
 	}
---
+++ Assets/Script/ArcadeKartRaycastSensor.cs
@@ -0,0 +1,78 @@
+/*
+SSBB4455 2020-08-01
+*/
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ArcadeKartRaycastSensor : MonoBehaviour
+{
+	Transform trans;
+	internal Transform Trans { get {
+			if (!trans)
+			{
+				trans = transform;
+			}
+			return trans;
+		}
+	}
+
+#region Senses
+	[Tooltip("How far should the agent shoot raycasts to detect the world?")]
+	public float raycastDistance;
+
+	[Tooltip("Which layers the raycasts will detect.")]
+	public LayerMask mask;
+
+	[Tooltip("Where the car edge?")]
+	public float carEdge = 0.03f;
+#endregion
+
+
+#region ShowRaycastMesh
+	public Mesh mesh;
+	public Material material;
+#endregion
+	
+
+#region Debugging
+	[Header("Debug Option")]
+	[Tooltip("Should we visualize the rays that the agent draws?")]
+	public bool ShowRaycasts;
+#endregion
+
+	float hitDistance;
+
+
+
+	public void Update()
+	{
+		if (ShowRaycasts)
+		{
+			Debug.DrawRay(Trans.position, Trans.forward * raycastDistance, Color.green);
+			Debug.DrawRay(Trans.position, Trans.forward * carEdge, Color.red);
+			//Graphics.DrawMesh(mesh, Vector3.zero, Quaternion.identity, material, 0);
+		}
+	}
+
+	public List<float> GetObservationV1()
+	{
+		bool hit = Physics.Raycast(Trans.position, Trans.forward, out RaycastHit hitInfo,
+			raycastDistance, mask, QueryTriggerInteraction.Ignore);
+		float hitDistance = hit ? hitInfo.distance : raycastDistance;
+		List<float> sensorRaycastInfoList = new List<float>();
+
+		// 检测器指向角度
+		sensorRaycastInfoList.AddRange(new float[3] { 
+			Trans.eulerAngles.normalized.x, Trans.eulerAngles.normalized.y, Trans.eulerAngles.normalized.z });
+		// 碰撞距离
+		sensorRaycastInfoList.Add(hitDistance / raycastDistance);
+		// 车身边缘
+		sensorRaycastInfoList.Add(carEdge / raycastDistance);
+		// 检测到的碰撞类型(墙壁0.1f 对方车辆0.2f 己方车辆0.3f)
+		sensorRaycastInfoList.Add(0.1f);
+
+		return sensorRaycastInfoList;
+	}
+
+}
