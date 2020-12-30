902af0773f4b7eb64ea81c3c268a4be412704582
SSB4455 <741086@gmail.com>
2020-12-30 17:07:11 +0800

    增加节日彩蛋(胖子跳舞)

---
+++ Assets/Script/Utility/EasterEggsScript.cs
@@ -0,0 +1,34 @@
+﻿/*
+SSBB4455 2020-12-30
+*/
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+	public class EasterEggsScript : MonoBehaviour
+	{
+		public GameObject easterEggsPrefab;
+		bool bigvegasDance;
+
+
+
+
+		void Awake()
+		{
+			if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "PlayingScene")
+			{
+				bigvegasDance = DateTime.Now.Month == 12 && DateTime.Now.Day == 25 ||
+					DateTime.Now.Minute % 7 == 1;
+			}
+		}
+
+		void Start()
+		{
+			if (bigvegasDance)
+			{
+				Instantiate(easterEggsPrefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
+			}
+		}
+	}
+}
